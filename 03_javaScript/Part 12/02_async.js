9b55539591321687526ec93caad94b714aef81d4
Sachin Kumar <sachin80510@gmail.com>
2026-08-16 12:59:56 +0530

    feat(js-async): implement async and await for clean promise handling

---
+++ 03_javaScript/Part 12/02_async.js
@@ -0,0 +1,13 @@
+async function greet(){
+//    throw "some random error"
+    return "hello!";
+}
+
+greet()
+  .then((result) => {
+    console.log("promise was resolved");
+    console.log("result was : ", result);
+  })
+  .catch((err) => {
+    console.log("promise was rejected with err : ", err);
+  });
