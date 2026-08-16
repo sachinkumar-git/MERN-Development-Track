086e67fe033b1719b2595eb0e54e39fa48d20c94
Sachin Kumar <sachin80510@gmail.com>
2026-08-16 12:59:16 +0530

    feat(js-async): explore javascript call stack and single thread execution

---
+++ 03_javaScript/Part 11/04_singleThread.js
@@ -0,0 +1,11 @@
+// let a = 25;
+// console.log(a);
+// let b = 10;
+// console.log(b);
+// console.log(a+b);
+
+setTimeout(()=> {
+    console.log("apna college");
+}, 2000);
+
+console.log("hello ... ")
