dec713511973cff4caeeef72c153a1c8ff759daf
Sachin Kumar <sachin80510@gmail.com>
2026-08-16 12:51:08 +0530

    feat(js-loops): implement basic for and while loops

---
+++ 03_javaScript/Part 3/02_forLoop.js
@@ -0,0 +1,35 @@
+// for (let i=1; i<=5; i++){
+//     console.log(i);
+// }
+
+
+// in decresing order 
+// for (let i=5; i>=1; i--){
+//     console.log(i);
+// }
+
+
+// for odd number 
+
+// method one
+// for (let i=1; i<=15; i++){
+//     if (i%2!=0){
+//         console.log(i);
+//     }
+// }
+
+
+// method two
+// for (let i=1; i<=15; i=i+2){
+    
+//         console.log(i);
+
+// }
+
+// print even number
+
+for (let i=2; i<=10; i++){
+    if (i%2==0){
+        console.log(i);
+    }
+}
---
+++ 03_javaScript/Part 3/03_multiplication.js
@@ -0,0 +1,11 @@
+
+// for (let i = 1 ; i<=10 ; i++){
+//     console.log(i, '*', 5 , '=',  i*5);
+// }
+
+let n = prompt("enter the number");
+n = parseInt(n); // to make integer
+
+for (let i = 1 ; i<=10 ; i++){
+    console.log(i*n);
+}
