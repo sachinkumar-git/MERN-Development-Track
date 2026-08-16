cc06697a9685c85111310e50e24596fd62d05480
Sachin Kumar <sachin80510@gmail.com>
2026-08-17 01:11:25 +0530

    feat(react-state): implement counter and like button to manage component state

---
+++ 04_React/react-state-class/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Counter from './counter';
+import LikeButton from './LikeButton';
+
+
+function App() {
+  return (
+    <>
+    <p>States in React</p>
+    <Counter/>
+    </>
+  );
+  
+}
+
+export default App
---
+++ 04_React/react-state-class/src/counter.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function init (){
+    console.log("init was called")
+    return Math.random();
+}
+
+export default function Counter() {
+  let [count, setCount] = useState(init); // initialization 
+    console.log ("component was rendered");
+
+
+  let incCount = () => {
+    setCount((currCount) => {
+        return currCount + 1;
+    });
+//    setCount((currCount) => {
+//     return currCount + 1;
+//    });
+// setCount(25);
+
+  };
+
+    return (
+        <div>
+            <h3>Count = {count}</h3>
+            <button onClick = {incCount} >Increase Count</button>
+
+        </div>
+    )
+
+}
