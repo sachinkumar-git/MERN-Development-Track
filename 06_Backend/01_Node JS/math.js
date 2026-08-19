2b6cc58689ed120ffb02c841fe1e2ddf75f70aa0
Sachin Kumar <sachin80510@gmail.com>
2026-08-19 22:21:46 +0530

    feat(node-basics): implement core node.js scripts and basic execution

---
+++ 06_Backend/01_Node JS/math.js
@@ -0,0 +1,30 @@
+const sum = (a, b) => a+b;
+const mul = (a, b) => a*b;
+const g = 9.8;
+const PI = 3.14;
+
+//method 1
+// let obj = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI,
+// };
+
+// module.exports = obj;
+
+
+//method 2
+// module.exports  = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI,
+// };
+
+
+// method 3
+// module.exports sum = (a, b) => a+b;
+
+// method 4
+// exports sum = (a, b) => a+b;
