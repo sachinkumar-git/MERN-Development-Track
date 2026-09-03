3b476ef63ae5f8a02cde16e9620e186058031917
Sachin Kumar <sachin80510@gmail.com>
2026-09-03 22:01:12 +0530

    feat(db-relations): create post model for database relationships

---
+++ 09_Database Relationships/Models/post.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+main()
+  .then(() => console.log("Connection successful"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
+}
+
+
+const userSchema = new Schema({
+  username: String,
+  email: String
+});
+
+const postSchema = new Schema({
+  content: String,
+  likes: Number,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+const Post = mongoose.model("Post", postSchema);
+
+const addData = async () => {
+  let user1 = new User({
+    username: "rahulkumar",
+    email: "rahul@gmail.com"
+  });
+
+  let post1 = new Post({
+    content: "Hello World!",
+    likes: 7
+  });
+
+  post1.user = user1;
+
+  await user1.save();
+  await post1.save();
+};
+
+addData();
