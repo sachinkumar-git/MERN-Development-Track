24ea78a844235215f74bc78d4e7f5d81c921b1e9
Sachin Kumar <sachin80510@gmail.com>
2026-09-03 21:54:29 +0530

    feat(mongodb-pt2): create database initialization script for sample data

---
+++ 07_mongodb/mongo Db Part 2/init.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js")
+
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
